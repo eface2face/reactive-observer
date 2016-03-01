0a1e766e9b3acaf2039e3bf6d24c81c8bfe04c7c
Iñaki Baz Castillo <ibc@aliax.net>
2016-03-01 11:03:13 +0100

    0.0.2

--- index.js
+++ index.js
@@ -1,154 +1,212 @@
-var debug = require('debug')('reactive-observer');
-
-
-module.exports = function(Meteor) {
-
+module.exports = function(Meteor)
+{
 	var Tracker = Meteor.Tracker;
 
+	/**
+	 * Construtor
+	 * @param {[Object]} object  If set it must be an EventEmitter
+	 */
 	function ReactiveObserver(object)
 	{
 		var root = null;
-		
-		// called without `new`
+
+		// Called without `new`
 		if (!(this instanceof ReactiveObserver))
 			return new ReactiveObserver(object);
-		
-		//Check object is obsevable
-		if (object && !object.listeners)
-			throw new Exception('Object is not observable');
-		
-		//Tracker dependencies
-		var dep  = new Tracker.Dependency;
-		
-		//Change listener
-		var listener = function() {
-			//And fire dependency
+
+		// Tracker dependencies
+		var dep = new Tracker.Dependency;
+
+		// Listener for 'change' event
+		function listener()
+		{
+			// And fire dependency
 			dep.changed();
 		};
 
-		// Gettter for root object or a property in it
-		this.get = function(key) {
-			//If running inside a an active Tracker computation
-			if(Tracker.active)
-				//Add a dependency
+		/**
+		 * Getter for the root object or a property in it
+		 * @param  {[any]} key  Root object property to get
+		 * @return {[any]}      The root object (may be undefined) if no `key` is given,
+		 *                      or the value of a property if `key` is given (undefined
+		 *                      if there is not root)
+		 */
+		this.get = function(key)
+		{
+			// If running inside a an active Tracker computation
+			if (Tracker.active)
 				dep.depend();
 
-			if(arguments.length === 0)
+			if (arguments.length === 0)
 				return root;
-			else
+			else if (root)
 				return root[key];
-				
+			else
+				return undefined;
 		};
 
-		// Settter for root object and properties
-		this.set = function()
+		/**
+		 * Setter for root object or properties
+		 * @param {[any]} key    The root propertyey to set or whole root object to set if `value`
+		 *                       if `value` is not given
+		 * @param {[any]} value  Property value if `key` is given
+		 */
+		this.set = function(key, value)
 		{
-			//If only called with one argument
-			if(arguments.length === 1)
+			switch (arguments.length)
 			{
-				//Unobserve object
-				root && root.removeListener('change',listener);
-				//Set root object
-				root = arguments[0];
-				//Check object is obsevable
-				if (root && !object.listeners)
-					throw new Exception('Object is not observable');
-				//Add listener
-				root.addListener('change',listener);
-				//And fire dependency
-				dep.changed();
-			} else if (root) {
-				//Get path, value and corresponding object
-				var key = arguments[0];
-				var value = arguments[1];
-				//Set property
-				root[key] = value;
-				//And fire dependency
-				dep.changed();
+				// Whole root object setter
+				case 1:
+				{
+					var object = key;
+
+					// Unobserve previous root object (if any)
+					if (root)
+						root.removeListener('change', listener);
+
+					// Check object is observable
+					if (!object.listeners)
+						throw new TypeError('given `object` is not an EventEmitter');
+
+					// Set root object
+					root = object;
+
+					// Add listener
+					root.addListener('change', listener);
+
+					// And fire dependency
+					dep.changed();
+
+					break;
+				}
+
+				// Property setter
+				case 2:
+				{
+					// If root is not set throw
+					if (!root)
+						throw new Error('root not set');
+
+					// Set property
+					root[key] = value;
+
+					// And fire dependency
+					dep.changed();
+
+					break;
+				}
+
+				default:
+				{
+					throw new TypeError('1 or 2 arguments must be given');
+				}
 			}
 		};
-		
-		this.has = function(key) {
-			//If running inside a an active Tracker computation
-			if(Tracker.active)
-				//Add a dependency
-				dep.depend();
 
-			if (!root)
-				return;
-
-			//Check if it has the key
-			return root.hasOwnProperty (key);
-		};
-		
-		this.keys = function() {
-			//If running inside a an active Tracker computation
-			if(Tracker.active)
-				//Add a dependency
-				dep.depend();
-
-			if (!root)
-				return [];
-
-			//Return object keys
-			return Object.keys(root);
-		};
-		
-		this.values = function() {
-			var values = [];
-			//If running inside a an active Tracker computation
-			if(Tracker.active)
-				//Add a dependency
-				dep.depend();
-
-			if (!root)
-				return [];
-
-			//For each key in object
-			for (var key in root)
-				if (root.hasOwnProperty (key))
-					//Add to values
-					values.push(root[key]);
-
-			//REturn keys
-			return values;
-		};
-		
-		this.entries = function() {
-			var entries = [];
-			//If running inside a an active Tracker computation
-			if(Tracker.active)
-				//Add a dependency
-				dep.depend();
-
-			if (!root)
-				return [];
-
-			//For each key in object
-			for (var key in root)
-				if (root.hasOwnProperty (key))
-					//Add to values
-					entries.push([key,root[key]]);
-			//REturn keys
-			return entries;
-		};
-
-		this.delete = function(key) {
+		/**
+		 * Delete given root property key
+		 * @param  {any} key
+		 */
+		this.delete = function(key)
+		{
 			if (!root)
 				return;
 
 			if (!root.hasOwnProperty(key))
 				return;
 
-			//Get corresponding object
-			var object = root[key];
-
-			// Remove from root object
+			// Remove property
 			delete root[key];
+
+			// And fire dependency
+			dep.changed();
 		};
-		
-		//Set object
-		this.set(object);
+
+		/**
+		 * Check whether root object has a property
+		 * @param  {[any]}  key
+		 * @return {Boolean}
+		 */
+		this.has = function(key)
+		{
+			// If running inside a an active Tracker computation
+			if (Tracker.active)
+				dep.depend();
+
+			if (!root)
+				return false;
+
+			return root.hasOwnProperty(key);
+		};
+
+		/**
+		 * Get root property keys
+		 * @return {Array}
+		 */
+		this.keys = function()
+		{
+			// If running inside a an active Tracker computation
+			if (Tracker.active)
+				dep.depend();
+
+			if (!root)
+				return [];
+
+			// Return object keys
+			return Object.keys(root);
+		};
+
+		/**
+		 * Get root property values
+		 * @return {Array}
+		 */
+		this.values = function()
+		{
+			// If running inside a an active Tracker computation
+			if (Tracker.active)
+				dep.depend();
+
+			if (!root)
+				return [];
+
+			var values = [];
+
+			for (var key in root)
+			{
+				if (root.hasOwnProperty(key))
+					values.push(root[key]);
+			}
+
+			return values;
+		};
+
+		/**
+		 * Return the list of root key/value
+		 * @return {Array}
+		 */
+		this.entries = function()
+		{
+			// If running inside a an active Tracker computation
+			if (Tracker.active)
+				dep.depend();
+
+			if (!root)
+				return [];
+
+			var entries = [];
+
+			for (var key in root)
+			{
+				if (root.hasOwnProperty(key))
+					entries.push([key, root[key]]);
+			}
+
+			return entries;
+		};
+
+		// Set root object (if given)
+		if (object)
+			this.set(object);
 	}
 
 	return ReactiveObserver;
